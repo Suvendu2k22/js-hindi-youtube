8f10474eabfece340c2b7959f084aef569d2bb83
Suvendu2k22 <114357832+Suvendu2k22@users.noreply.github.com>
2024-12-28 06:49:37 +0000

    uodateddd

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail= "suv@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId= 2
+// accountEmail="dshjb@jnb.com"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+/*
+prefer not to use var 
+bacause of issue in block scope and functional scope
+*/
+// console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail="sd@cnkj.com"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
 console.log("suv");
+console.log("suv");
