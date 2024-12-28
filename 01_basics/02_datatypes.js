30e29b74166784745850ce5936150786be22ef12
Suvendu2k22 <114357832+Suvendu2k22@users.noreply.github.com>
2024-12-28 12:59:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all javascript code as newerr version
+
+
+// alert(3+3)   we are using node js not browser
+
+console.log(3
+    +3) //code readability should be high
+
+
+let name = "suv"
+let age = 18
+// let isLoggedIn= flase
+
+
+// number => 2 to power 53
+// bigint
+// string => "suv"
+// boolean => true/false
+// null =>standlone value
+// undefined =>
+// symbol  =>unique
+
+
+// object 
+
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
